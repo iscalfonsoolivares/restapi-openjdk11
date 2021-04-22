bfdaa544a8c9fc47d164ce62749e8d752fe4a1d7
John Doe <johndoe@example.com>
2021-04-22 08:25:51 -0500

    first commit

---
+++ src/main/java/com/aowebdev/restapiopenjdk11/RestapiOpenjdk11Application.java
@@ -0,0 +1,13 @@
+package com.aowebdev.restapiopenjdk11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestapiOpenjdk11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestapiOpenjdk11Application.class, args);
+	}
+
+}
